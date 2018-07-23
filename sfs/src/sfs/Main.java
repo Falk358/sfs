bea0c9a510c6b52af30ac9998f18a4a2cca910a2
David <thecharonlp@gmail.com>
2018-07-23 10:59:44 +0200

    added third test tile

--- sfs/src/sfs/Main.java
+++ sfs/src/sfs/Main.java
@@ -20,8 +20,11 @@ public class Main {
     public static void main(String[] args) {
         Tile tile1 =new Tile("test room");
         Tile tile2 =new Tile("test room 2");
+        Tile tile3 =new Tile("test room 3");
         tile1.setTile_north(tile2);
         tile2.setTile_south(tile1);
+        tile1.setTile_east(tile3);
+        tile3.setTile_west(tile1);
         Player player =new Player(tile1);
         Game game=new Game(player);
         game.start();
